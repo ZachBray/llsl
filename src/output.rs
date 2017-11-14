0044fc750dce4939700a3a7a414c05d3af780948
Zach Bray <zachbray@googlemail.com>
2017-11-14 22:28:30 +0000

    refactor(generation): Makes it possible to generate multiple targets with a single template.

--- src/output.rs
+++ src/output.rs
@@ -1,32 +1,32 @@
 use std::path::{Path, PathBuf};
 use std::fs::{File, create_dir_all};
 use handlebars::Handlebars;
+use serde::Serialize;
 use super::try::*;
 use super::model::*;
 
-struct Template {
-    name: &'static str,
-    content: &'static str,
-    output_file: &'static str,
+struct TemplateRenderer<'a> {
+    output_dir: &'a str,
+    template: &'a Template,
+    engine: &'a Handlebars,
 }
 
-impl Template {
-    fn execute(&self, engine: &mut Handlebars, protocol: &Protocol, output_dir: &str) -> Try<()> {
-        info!("Executing {} template", self.name);
-        let output_path = self.resolve_output_path(output_dir);
-        trace!("Template: {}", self.content);
-        let mut output_file = Template::create_output_file(&output_path)?;
-        engine
-            .template_renderw(self.content, protocol, &mut output_file)
+impl<'a> TemplateRenderer<'a> {
+    fn render<T>(&self, output_path: &str, data: &T) -> Try<()>
+    where
+        T: Serialize,
+    {
+        info!("Executing {} template", self.template.name);
+        let output_path = Path::new(self.output_dir).join(output_path);
+        trace!("Template: {}", self.template.content);
+        let mut output_file = TemplateRenderer::create_output_file(&output_path)?;
+        self.engine
+            .template_renderw(self.template.content, data, &mut output_file)
             .map_err(|e| ErrorCode::FailedToExecuteTemplate(e))
     }
 
-    fn resolve_output_path(&self, output_dir: &str) -> PathBuf {
-        Path::new(output_dir).join(self.output_file)
-    }
-
     fn create_output_file(output_path: &PathBuf) -> Try<File> {
-        Template::create_parent_dir(output_path)?;
+        TemplateRenderer::create_parent_dir(output_path)?;
         debug!("Creating output file: {:?}", output_path);
         File::create(output_path).map_err(|e| ErrorCode::FailedToCreateOutputFile(e))
     }
@@ -42,18 +42,31 @@ impl Template {
     }
 }
 
-static TEMPLATES: [Template; 1] = [
-    Template {
-        name: "readme",
-        content: include_str!("../templates/readme.hbs"),
-        output_file: "README.md",
-    },
-];
+struct Template {
+    name: &'static str,
+    content: &'static str,
+    render_targets: Box<Fn(&Protocol, &mut TemplateRenderer) -> Try<()>>,
+}
+
+fn templates() -> [Template; 1] {
+    [
+        Template {
+            name: "readme",
+            content: include_str!("../templates/readme.hbs"),
+            render_targets: Box::new(|protocol, renderer| renderer.render("README.md", protocol)),
+        },
+    ]
+}
 
 pub fn generate_code(protocol: &Protocol, output_dir: &str) -> Try<()> {
-    let mut engine = Handlebars::new();
-    for t in TEMPLATES.iter() {
-        t.execute(&mut engine, protocol, output_dir)?;
+    let engine = Handlebars::new();
+    for template in templates().iter() {
+        let mut renderer = TemplateRenderer {
+            output_dir,
+            engine: &engine,
+            template,
+        };
+        (*template.render_targets)(protocol, &mut renderer)?;
     }
     Ok(())
 }
